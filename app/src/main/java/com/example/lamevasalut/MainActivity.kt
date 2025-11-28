d240aea765ec23de6a90f9343ea2001ff184142e
toniaguilar <toni.aguilar@iticbcn.cat>
2025-11-28 08:48:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lamevasalut/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.lamevasalut
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val dadesSalut = listOf(
+        Pair("Distància caminant o corrent" , "11,6 km"),
+        Pair("Passos" , "14.320"),
+        Pair("Pes" , "69,3 kg"),
+        Pair("Pressió arterial" , "133/78 mmHg"),
+        Pair("Freqüència cardíaca en repòs" , "62 bpm"),
+        Pair("Freqüència cardíaca màxima" , "152 bpm"),
+        Pair("Calories cremades" , "2450 kcal"),
+        Pair("Son total" , "7 h 42 min"),
+        Pair("Son profund" , "1 h 18 min"),
+        Pair("Oximetria" , "97%"),
+        Pair("Glucosa en sang" , "92 mg/dL"),
+        Pair("Temperatura corporal" , "36,6 ºC"),
+        Pair("IMC" , "22,3"),
+        Pair("Nivell d’estrès" , "Baix"),
+        Pair("Altitud acumulada pujada" , "184 m"),
+        Pair("Nivell d’hidratació" , "1,9 L"),
+        Pair("Cicles cardíacs irregulars detectats" , "0"),
+        Pair("Temps d’exercici" , "46 min"),
+        Pair("VO₂ Max" , "46 ml/kg/min"),
+        Pair("Grasa corporal" , "16,4%"),
+        Pair("Massa muscular" , "52,1 kg")
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lamevasalut/ProvaAvaluacioPSAM.kt
@@ -0,0 +1,62 @@
+package com.example.lamevasalut
+
+/*
+PREGUNTA 01
+Defineix i activa una barra de desplaçament (ScrollBar) a l'aplicació LaMevaSalud.
+Fes que sigui vissible
+
+PREGUNTA 02
+L'aplicació comença per la pantalla activity_main, però vull que abans de mostrar
+aquesta pantalla es mostri la pantalla de login (login_activity). Modifica el que
+consideris convenient.
+
+PREGUNTA 03
+Necessito definir les següents categories d'informació mèdica:
+- Activitat
+- Audició
+- Benestar Mental
+- Constants
+- Control de cicle
+- Cor
+- Medicació
+- Mesures corporal
+- Altres dades
+Com ho faries?
+
+PREGUNTA 04
+A partir d'un array de parells, genera la pantalla inicial de la meva salut
+
+val dadesSalut = listOf(
+    Pair("Distància caminant o corrent" , "11,6 km"),
+    Pair("Passos" , "14.320"),
+    Pair("Pes" , "69,3 kg"),
+    Pair("Pressió arterial" , "133/78 mmHg"),
+    Pair("Freqüència cardíaca en repòs" , "62 bpm"),
+    Pair("Freqüència cardíaca màxima" , "152 bpm"),
+    Pair("Calories cremades" , "2450 kcal"),
+    Pair("Son total" , "7 h 42 min"),
+    Pair("Son profund" , "1 h 18 min"),
+    Pair("Oximetria" , "97%"),
+    Pair("Glucosa en sang" , "92 mg/dL"),
+    Pair("Temperatura corporal" , "36,6 ºC"),
+    Pair("IMC" , "22,3"),
+    Pair("Nivell d’estrès" , "Baix"),
+    Pair("Altitud acumulada pujada" , "184 m"),
+    Pair("Nivell d’hidratació" , "1,9 L"),
+    Pair("Cicles cardíacs irregulars detectats" , "0"),
+    Pair("Temps d’exercici" , "46 min"),
+    Pair("VO₂ Max" , "46 ml/kg/min"),
+    Pair("Grasa corporal" , "16,4%"),
+    Pair("Massa muscular" , "52,1 kg")
+)
+
+PREGUNTA 05
+A la pantaLla de login, defineix un bottom menú amb sis elements.
+Tens imatges svg a la carpeta res\drawable\resources
+
+ */
+
+
+
+class ProvaAvaluacioPSAM {
+}
